929e43dd104a0db2df10638eef86fa419cecd4a2
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-19 16:19:21 -0300

    feat: "reload"

---
+++ src/components/Form/index.js
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import React from 'react'
+export default function Form() {
+    return (
+        <View>
+            <View>
+                <Text style={styles.title}>Altura</Text>
+                <TextInput style={styles.inputs}
+                placeholder='Your altura'></TextInput>
+                <Text style={styles.title}>Peso</Text>
+                <TextInput style={styles.inputs}
+                placeholder='Your peso'></TextInput>
+                
+                <Button title="Calcular IMC"/>
+            </View>
+
+            <ResultIMC/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    inputs: {
+        fontSize: 24,
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#000',
+        paddingHorizontal: 30,
+        borderRadius: 10,
+    },
+
+    title: {
+        fontSize: 30,
+      },
+      
+    
+  });
+  
